5b4115ba5d4a3808147d8b0b0cc50be76c0026f7
Ricardo Takuno <takunoricardo@gmail.com>
2017-09-05 21:49:47 -0300

    Aplicativo TextDrive

---
+++ TestDrive/TestDrive/TestDrive/Views/AgendamentView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestDrive.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AgendamentView : ContentPage
+	{
+        public Veiculo Veiculo { get; set; }
+        public string Nome { get; set; }
+        public string Fone { get; set; }
+        public string Email { get; set; }
+        public DateTime DataAgendamento { get; set; }
+        public TimeSpan HoraAgendamento { get; set; }
+		public AgendamentView (Veiculo veiculo)
+		{
+			InitializeComponent ();
+		}
+	}
+}
